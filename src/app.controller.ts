6a3a1935903e48e934d3c00f1bc4c144e1232bd7
Jean-Michel GRONDIN <jean-michelgrondin@pop-os.localdomain>
2021-04-14 16:27:36 +0400

    fix comments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Req,  Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Request, Response } from 'express';
-import { Interface } from 'node:readline';
 
 @Controller()
 export class AppController {
@@ -11,17 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  // @Get('AllTickets')
-  // AllTickets() {//@Res() res: Response
-  //   // return res.status(200).json({ msg: "This route return all tickets", test: request.body, testTwo: request.body.servicename});
-  //   return { msg: "This route return all tickets", req: "request" };
-  // }
 }
-
-
-// @Post('AllTickets')
-// AllTickets(@Req() request: Request, @Res() res: Response) {
-//   return res.status(201).json({ msg: "This route return all tickets", test: request.body, testTwo: request.body.servicename});
-//   // return { msg: "This route return all tickets", req: request };
-// }
