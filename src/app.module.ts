23bf09d2f27fc940ac5c65c43a3640fa9be04f3f
Jean-Michel GRONDIN <jean-michelgrondin@pop-os.localdomain>
2021-04-23 13:24:03 +0400

    remove db infos even if i changed them

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { TicketsModule } from './tickets.module';
 
 @Module({
     imports: [
-            MongooseModule.forRoot('mongodb+srv://jmAdmin:oKP3C7iiCoh7yKMA@jm-experiment-database.4h4xe.mongodb.net/testyocto?retryWrites=true&w=majority'),
+            MongooseModule.forRoot('mongodb+srv://DB_USERNAME:DM_PASSWORD@DB_NAME/testyocto?retryWrites=true&w=majority'),
             TicketsModule
         ],
     controllers: [AppController],
